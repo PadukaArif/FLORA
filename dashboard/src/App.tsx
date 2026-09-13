02a4bc491160a72ab77c728f6b75c3712fe89870
aethereal123 <adzanahlilfiqri@gmail.com>
2026-09-13 16:37:29 +0700

    fix(dashboard): remove obsolete onReplayIntro prop from Header

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -81,7 +81,6 @@ export const App: React.FC = () => {
           <main className="flex-1 min-w-0 p-4 sm:p-6 lg:p-8 max-w-[1440px] mx-auto w-full">
             <Header
               onRefresh={refresh}
-              onReplayIntro={() => setExpStage('boot')}
               onOpenMobileMenu={() => setMobileMenuOpen(true)}
               mqttStatus={data?.mqtt}
               isStale={isStale}
